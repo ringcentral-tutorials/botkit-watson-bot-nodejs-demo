15b3dda7d790fd44f59fce21a7ac7a49118815f3
Pawan <pkvenu@gmail.com>
2018-01-04 14:45:10 -0800

    Updated to work with new onboarding flow

--- index.js
+++ index.js
@@ -9,33 +9,60 @@ var Botkit= require('botkit'),
     os = require('os'),
     http = require('http'),
     request = require('request'),
-    watson = require('watson-developer-cloud');
+    watson = require('watson-developer-cloud'),
+    fs = require('fs');
+
+var accessToken = "";
+var platform = null;
+
+if (!process.env.GLIP_CLIENT_ID || !process.env.GLIP_CLIENT_SECRET || !process.env.GLIP_PORT) {
+    console.log('Error: Specify clientId clientSecret and port in environment');
+    process.exit(1);
+}
 
 var controller = Botkit.glipbot({
-    debug: false
+    debug: true
+}).configureGlipApp({
+    clientId: process.env.GLIP_CLIENT_ID,
+    clientSecret: process.env.GLIP_CLIENT_SECRET,
+    redirectUri: process.env.OAUTH_REDIRECT_URI,
+    apiRoot: process.env.GLIP_SERVER
 });
 
-var bot = controller.spawn({
-    server: process.env.GLIP_SERVER,
-    appKey: process.env.GLIP_APPKEY,
-    appSecret: process.env.GLIP_APPSECRET,
-    username: process.env.GLIP_USERNAME,
-    password: process.env.GLIP_PASSWORD,
-    extension: process.env.GLIP_EXTENSION
-}).startRTM();
+readAccessToken()
 
-controller.setupWebserver(process.env.port || 3000, function(err, webserver){
+function readAccessToken(){
+  try {
+    fs.accessSync('token.dat');
+    accessToken = fs.readFileSync('token.dat', 'utf8');
+  }catch (e) {
+    accessToken = ""
+  }
+}
 
-    if(!err){
-        console.log('error' + err);
-    }
+function storeAccessToken(accessToken){
+  fs.writeFile('token.dat', accessToken, function(err) {
+    if(err)
+      console.log(err)
+  })
+}
 
-    webserver.get('/', function (req ,res) {
-        res.send(':)');
+var bot = controller.spawn({});
+
+controller.setupWebserver(process.env.GLIP_PORT || 3000, function(err, webserver){
+    controller.createWebhookEndpoints(webserver, bot,  function () {
+        console.log("Online");
     });
 
-
-    controller.createWebhookEndpoints(webserver, bot);
+    controller.createOauthEndpoints(webserver, bot, accessToken, function(err, req, res, token) {
+        if(err) {
+            res.status(500).send('ERROR: ' + err);
+        } else {
+            platform = controller.getRCPlatform();
+            storeAccessToken(token);
+            //res.send('Success!');
+        }
+    })
 
 });
 
@@ -43,19 +70,24 @@ var personality_insight = watson.personality_insights({
     username: process.env.WATSON_USERNAME,
     password: process.env.WATSON_PASSWORD,
     url: process.env.WATSON_URL,
-    version: process.env.WATSON_VERSION
+    version: process.env.WATSON_VERSION,
+    version_date: process.env.WATSON_VERSION_DATE
 });
 
 controller.hears(['watson: analyze'], 'message_received', function (bot,message) {
-    //console.log("In Hears:" + JSON.stringify(message));
-    bot.api.posts().get({groupId: message.channel}).then(function(history) {
 
+    console.log("In Hears:" + JSON.stringify(message));
+
+    controller.getRCPlatform().get('/glip/posts',{groupId: message.channel}).then(function(history) {
+        console.log("History: " + history.json());
         var messages = [];
-        for(var i=0; i < history.records.length; i++){
-            messages.push(history.records[i].text);
+        var data = history.json();
+        for(var i=0; i < data.records.length; i++){
+            messages.push(data.records[i].text);
         }
 
         var corpus = messages.join("\n");
+        console.log(corpus);
 
         personality_insight.profile(
             {
@@ -67,7 +99,7 @@ controller.hears(['watson: analyze'], 'message_received', function (bot,message)
                     console.log('error:', err)
                 }else{
                     controller.startTask(bot, message, function (task, convo) {
-                        var top5 = response.tree.children[0].children[0].children;
+                        var top5 = response.personality;
 
                         for (var c = 0; c <  top5.length; c++) {
 
@@ -80,4 +112,3 @@ controller.hears(['watson: analyze'], 'message_received', function (bot,message)
         )
     });
 });
-
